adfc4a04f3452e6fd05c711edc0bb56156733a0e
Denis Nikhaev <denis.nikhaev@gmail.com>
2025-10-26 21:39:36 +0300

    created router for 'api/runs', RunViewSet

--- project_run/urls.py
+++ project_run/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from app_run.views import company_details
+from rest_framework.routers import DefaultRouter
+from app_run.views import RunViewSet
+
+router = DefaultRouter()
+router.register('api/runs', RunViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/company_details/', company_details)
+    path('api/company_details/', company_details),
+    path('', include(router.urls))
 ]
