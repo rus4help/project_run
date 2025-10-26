86bf29d4abfa246ced36edb8630b20dcde819a0f
Denis Nikhaev <denis.nikhaev@gmail.com>
2025-10-26 21:35:06 +0300

    created class RunViewSet

--- app_run/views.py
+++ app_run/views.py
@@ -2,6 +2,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
 
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
+
 
 @api_view(['GET'])
 def company_details(request):
@@ -9,3 +13,8 @@ def company_details(request):
                'slogan': settings.SLOGAN,
                'contacts': settings.CONTACTS}
     return Response(details)
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
