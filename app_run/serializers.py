aafd5e9b1c379ba01e952e5988d5f5e303b8f6d9
Denis Nikhaev <denis.nikhaev@gmail.com>
2026-01-11 15:58:33 +0300

    added field runs_finished into UserSerializer and counting runs

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -5,14 +5,18 @@ from .models import Run
 
 class UserSerializer(serializers.ModelSerializer):
     type = serializers.SerializerMethodField() # Тут мы задаем новое поле, которого нет в модели БД
+    runs_finished = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ['id', 'date_joined', 'username', 'first_name', 'last_name', 'type'] # Здесь добавили данное поле type
+        fields = ['id', 'date_joined', 'username', 'first_name', 'last_name', 'type', 'runs_finished'] # Здесь добавили данное поле type и runs_finished
 
     # Определяем метод, который вычисляет значение поля
     def get_type(self, obj):
         return 'coach' if obj.is_staff else 'athlete'
+
+    def get_runs_finished(self, obj):
+        return obj.runs.filter(status="finished").count()
 
 
 # Сериализатор только для данных атлета (минималистичный)
